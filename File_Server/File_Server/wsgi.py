29206648a0bed32f9e51ca62c778570c9296bfc2
Ravin D <ravin.d3107@outlook.com>
2023-12-21 11:02:13 +0530

    Configuring Vercel.json and build_files.sh

--- File_Server/File_Server/wsgi.py
+++ File_Server/File_Server/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'File_Server.settings')
 
 application = get_wsgi_application()
+
+app = application
